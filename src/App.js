0a30144f0a94ea1c10d63b68cee9657015e50f59
Samarth <hegdesamarth108@gmail.com>
2024-06-06 09:40:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Other routes */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
